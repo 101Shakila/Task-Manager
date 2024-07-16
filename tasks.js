f5bbc442d4d18ad4fd8fc06bb50e6057fc2d0c92
Shakila101 <167652608+101Shakila@users.noreply.github.com>
2024-07-16 15:46:01 -0400

    added event listener

---
+++ tasks.js
@@ -0,0 +1,3 @@
+//We will make a event listener where it will CALL the FUNCTION "addUpTask" when we click on submit BUTTON
+document.getElementbyID('taskContainer').addEventListener('submit', addUpTask);
+

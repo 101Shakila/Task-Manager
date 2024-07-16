cc3f064ca92ce9fedd0aa30d5a0126e2ddc44206
Shakila101 <167652608+101Shakila@users.noreply.github.com>
2024-07-16 16:05:54 -0400

    taskCollection function

--- tasks.js
+++ tasks.js
@@ -1,3 +1,20 @@
 //We will make a event listener where it will CALL the FUNCTION "addUpTask" when we click on submit BUTTON
 document.getElementbyID('taskContainer').addEventListener('submit', addUpTask);
 
+//Task manager contains mulitple tasks - hence we need to save it into an array
+let tasksCollection = [];
+
+//Lets create a function where it gathers the users input and pushes it into the tasksCollection array.
+function addUpTask(e) {
+    e.preventDefault();  //this will prevent the deafult behavior of form submission ( in this case ) and follow our javascript code instead
+
+    //lets store user input into variables
+    const taskDescription = document.getElementById('description').value;
+    const assignedTo = document.getElementById('assignedTo').value;
+    const dueDate = document.getElementById('dueDate').value;
+    const priority = document.getElementById('priority').value;
+    const status = document.getElementById('status').value;
+
+    //we will store the data into a object - this way we can store key value pairs
+    //We want to store mulitple objects into a array so we can use methods such as push, forEach etc..
+}
